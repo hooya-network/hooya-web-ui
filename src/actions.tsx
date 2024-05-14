980b51f89ba2e3e0cda77f8c266e3004e22bcbf0
Wesley Coakley <w@wesl.ee>
2024-05-14 16:06:35 -0400

    handle cases with multiple search terms during suggest phase

--- src/actions.tsx
+++ src/actions.tsx
@@ -17,13 +17,27 @@ export async function QuerySuggest(term: string, suggestions: 10) {
 
   const data: SuggestTagResponse = await res.json()
 
-  return data
+  const tagSuggestions = data
+    .tag_suggestion
     .sort((a, b) => b.count - a.count)
     .map((s) => [s.namespace, s.descriptor].join(":"))
+
+  const tagConstraints = data.tag_constraints
+    .map((c) => [c.descriptor, c.namespace].join(":"))
+
+  return tagSuggestions
+    .map((s) => tagConstraints.length ? [tagConstraints?.join(","), s].join(",") : s)
 }
 
 type SuggestTagResponse = {
-  namespace: string,
-  descriptor: string,
-  count: number,
-}[]
+  tag_suggestion: {
+    namespace: string,
+    descriptor: string,
+    count: number,
+  }[]
+  tag_constraints: {
+    namespace: string,
+    descriptor: string,
+    negated: boolean,
+  }[]
+}
