9630c8fd3a6da5cf5e5d71be8e534abe26464787
Wesley Coakley <wesley@skip.money>
2025-01-21 21:22:05 -0800

    fix flake

--- flake.nix
+++ flake.nix
@@ -1,35 +1,30 @@
 {
   inputs = {
-    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    systems,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    eachSystem = f:
-      nixpkgs.lib.genAttrs (import systems) (
-        system:
-          f nixpkgs.legacyPackages.${system}
-      );
-  in {
-    devShells = eachSystem (pkgs: {
-      default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodejs
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { system = system; };
+        isDarwin = pkgs.stdenv.isDarwin;
+      in {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+          nodejs
           # You can set the major version of Node.js to a specific one instead
           # of the default version
           # pkgs.nodejs-19_x
 
           # You can choose pnpm, yarn, or none (npm).
-          pkgs.nodePackages.pnpm
+          nodePackages.pnpm
           # pkgs.yarn
 
-          pkgs.nodePackages.typescript
-          pkgs.nodePackages.typescript-language-server
-        ];
-      };
-    });
-  };
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+          ] ++ pkgs.lib.optional isDarwin (with darwin.apple_sdk.frameworks; [ Security CoreServices ]);
+        };
+      }
+    );
 }
